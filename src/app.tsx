939e750523889e638a5c877a5903b1f472922fe7
Juan Veronez <juan.monteirov@gmail.com>
2025-08-24 19:51:29 -0300

    feat: initial components for upload widget and global styles

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,10 @@
+import { UploadWidget } from "./components/upload-widget";
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    // Why dvh? https://web.dev/blog/viewport-units?hl=pt-br
+    <main className="h-dvh flex flex-col justify-center items-center p-10">
+      <UploadWidget />
+    </main>
+  )
 }
