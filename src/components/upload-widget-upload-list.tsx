2a5f127b8bc885d8843bb87eac9417706b5b6db5
Juan Veronez <juan.monteirov@gmail.com>
2025-09-03 06:46:33 -0300

    feat: update widget overflow/scroll

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -8,7 +8,7 @@ export function UploadWidgetUploadList() {
   const isUploadListEmpty = uploadListSize === 0
 
   return (
-    <div className="px-3 flex flex-col gap-3">
+    <div className="px-3 flex flex-col gap-3 h-full overflow-hidden">
       <span className="text-sm font-medium">
         Uploaded files{' '}
         <span className="text-zinc-400">({uploadListSize})</span>
@@ -17,7 +17,7 @@ export function UploadWidgetUploadList() {
       {isUploadListEmpty ? (
         <span className="text-xs text-zinc-400">No uploads added</span>
       ) : (
-        <div className="space-y-2">
+        <div className="space-y-2 overflow-auto">
           {Array.from(uploads.entries()).map(([uploadId, upload]) => (
             <UploadWidgetUploadItem key={uploadId} upload={upload} />
           ))}
