38f8deec4333e54b6d52ffc52d6eb8fff17d637b
Juan Veronez <juan.monteirov@gmail.com>
2025-09-04 06:42:40 -0300

    feat: useRef to get dynamic height for upload widget

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -5,8 +5,10 @@ import { UploadWidgetUploadList } from "./upload-widget-upload-list";
 import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 import { motion, useCycle } from "motion/react";
 import { usePendingUploads } from "../store/uploads";
+import { useRef } from "react";
 
 export function UploadWidget() {
+  const contentRef = useRef<HTMLDivElement>(null)
   const { isThereAnyPendingUploads } = usePendingUploads()
 
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true)
@@ -20,22 +22,22 @@ export function UploadWidget() {
           closed: {
             width: 'max-content',
             height: 44,
-            transition: { type: 'inertia' }
+            transition: { duration: 0.1 }
           },
           opened: {
             width: 360,
-            height: 'auto',
+            height: contentRef.current?.clientHeight,
             transition: { duration: 0.1 }
           }
         }}
         className={`
-          bg-zinc-900 w-[360px] overflow-hidden rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border
+          bg-zinc-900 overflow-hidden rounded-xl data-[state=open]:shadow-shape border border-transparent animate-border
           data-[state=closed]:rounded-3xl data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,theme(colors.zinc.900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.zinc.700/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.zinc.600/.48))_border-box]
         `}
       >
         {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
-        <Collapsible.Content>
+        <Collapsible.Content ref={contentRef}>
           <UploadWidgetHeader />
 
           <div className="flex flex-col gap-4 py-3">
